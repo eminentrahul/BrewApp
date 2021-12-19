e9705edada514eeffd76e654f52a2a5753037a40
Rahul Ravi Prakash <eminentrahul@rocketmaiil.com>
2021-12-19 11:01:03 +0530

    Initial Commit

---
+++ BrewApp/BrewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewAppApp.swift
+//  BrewApp
+//
+//  Created by Rahul Ravi Prakash on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BrewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrewApp
+//
+//  Created by Rahul Ravi Prakash on 19/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
